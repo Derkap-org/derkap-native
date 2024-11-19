19aa4812145f950670a39dbb722a7bb1caf98ec1
Nico <nicobdx@icloud.com>
2024-11-19 20:02:42 +0100

    add camera component and layout

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-
+import { Link } from 'expo-router';
 const Home = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-3xl text-red-700">Home</Text>
+      <Link href="/camera" className="text-blue-700">Go to Camera</Link>
     </View>
   )
 }
