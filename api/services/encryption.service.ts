99dc209b7a1a03db42a176678338f34d988fe3f2
Nico <nicobdx@icloud.com>
2025-02-08 19:24:46 +0100

    new encryption service

--- api/services/encryption.service.ts
+++ api/services/encryption.service.ts
@@ -1,38 +1,61 @@
 import crypto from 'crypto'
-const ALGORITHM = 'aes-256-cbc'
-const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY!
+import { getSupabaseClient } from './supabase.service';
+const SERVER_PRIVATE_KEY = "seerver-key" //process.env.SERVER_PRIVATE_KEY!
 
-export const encrypt = ({
-  buffer,
-}: {
-  buffer: Buffer
-}): { encryptedData: string; iv: string } => {
-  const iv = crypto.randomBytes(16)
-  const cipher = crypto.createCipheriv(
-    ALGORITHM,
-    Buffer.from(ENCRYPTION_KEY, 'hex'),
-    iv
-  )
-
-  const encryptedData = Buffer.concat([cipher.update(buffer), cipher.final()])
-
-  return {
-    encryptedData: encryptedData.toString('base64'),
-    iv: iv.toString('base64'),
-  }
+export const generateEncryptionKey = (baseKey: string): string => {
+  const hmac = crypto.createHmac('sha256', SERVER_PRIVATE_KEY);
+  hmac.update(baseKey);
+  const digest =  hmac.digest();
+  return digest.toString('hex');
 }
 
-export const decrypt = (encryptedData: string, iv: string): Buffer => {
-  const decipher = crypto.createDecipheriv(
-    ALGORITHM,
-    Buffer.from(ENCRYPTION_KEY, 'hex'),
-    Buffer.from(iv, 'base64')
-  )
+export const getEncryptionKey = async ({challengeId, groupId, access_token, refresh_token}:{challengeId:string, groupId:string; access_token:string, refresh_token:string}) => {
+  const supabase = await getSupabaseClient({access_token, refresh_token})
+  const user_id = (await supabase.auth.getSession()).data.session?.user.id
+  if (!user_id) {
+    throw new Error('User not found')
+  }
+  // check if challenge is valid for the group
+  const { data: challenges, error } = await supabase
+    .from('challenge')
+    .select('id, base_key')
+    .eq('id', challengeId)
+    .eq('group_id', groupId)
+    .order('created_at', { ascending: false })
+    .limit(1)
 
-  const decryptedData = Buffer.concat([
-    decipher.update(Buffer.from(encryptedData, 'base64')),
-    decipher.final(),
-  ])
+  if (error) {
+    throw new Error(error.message)
+  }
 
-  return decryptedData
+  if (!challenges || challenges.length === 0) {
+    throw new Error('Challenge not found')
+  }
+
+  // check if the user is in the group
+  const { data: groupProfiles, error: groupProfilesError } = await supabase
+    .from('group_profile')
+    .select('id')
+    .eq('group_id', groupId)
+    .eq('profile_id', user_id)
+    .order('created_at', { ascending: false })
+    .limit(1)
+
+  if (groupProfilesError) {
+    throw new Error(groupProfilesError.message)
+  }
+
+  if (!groupProfiles || groupProfiles.length === 0) {
+    throw new Error('User not in the group')
+  }
+
+  const challenge = challenges[0]
+  const challenge_base_key = challenge.base_key
+  if (!challenge_base_key) {
+    throw new Error('Challenge base key not found')
+  }
+
+  const encryptionKey = generateEncryptionKey(challenge_base_key)
+  return encryptionKey
+
 }
--- api/services/supabase.service.ts
+++ api/services/supabase.service.ts
@@ -2,13 +2,9 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.SUPABASE_URL!
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!
+
 export const getSupabaseClient = async ({access_token, refresh_token}:{access_token:string, refresh_token:string}) => {
   const supabase =  createClient(supabaseUrl, supabaseAnonKey)
   supabase.auth.setSession({access_token, refresh_token})
   return supabase 
-}
-
-export const getSupabaseClientAdmin = async () => {
-  const supabase =  createClient(supabaseUrl, supabaseAnonKey)
-  return supabase
 }
