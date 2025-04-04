a28b9c0b181bf527b79d700e8463d40d2ad85e8f
Thomas Aubert <90507019+N0rooo@users.noreply.github.com>
2025-04-04 11:54:09 +0200

    :zap: improve(notification): improve post notifs + update package

--- application/supabase/functions/notify-new-post/index.ts
+++ application/supabase/functions/notify-new-post/index.ts
@@ -1,14 +1,12 @@
 import { createClient } from "jsr:@supabase/supabase-js@2";
 import { sendNotification } from "../_shared/send-notification.ts";
 
-type Challenge = {
-  group_id: number;
-  group: {
-    name: string;
-  };
+type Record = {
+  derkap_id: string;
+  allowed_user_id: string;
 };
 
-type Sender = {
+type Profile = {
   username: string;
 };
 
@@ -22,86 +20,66 @@ const supabase = createClient(
 );
 
 Deno.serve(async (req) => {
-  const { post_id, challenge_id, sender_id } = await req.json();
-  console.log(post_id, challenge_id, sender_id);
+  const { record } = (await req.json()) as { record: Record };
+  console.log(record);
 
-  const { data: challengeData, error: challengeError } = await supabase
-    .from("challenge")
+  const derkap_id = record.derkap_id;
+  const allowed_user_id = record.allowed_user_id;
+
+  console.log(allowed_user_id, derkap_id);
+
+  const { data: derkapData, error: derkapError } = await supabase
+    .from("derkap")
     .select(
       `
-        group_id,
-        group:group_id (
-          name
+        profile!derkap_creator_id_fkey(
+          username
         )
       `,
     )
-    .eq("id", challenge_id)
+    .eq("id", derkap_id)
     .single();
 
-  if (challengeError) {
-    console.error("Error fetching challenge", challengeError);
-    throw challengeError;
+  if (derkapError) {
+    console.error("Error fetching derkap", derkapError);
+    throw derkapError;
   }
 
-  const challenge = challengeData as unknown as Challenge;
-
-  const { data: senderData, error: senderError } = await supabase
-    .from("profile")
-    .select("username")
-    .eq("id", sender_id)
-    .single();
-
-  if (senderError) {
-    console.error("Error fetching sender", senderError);
-    throw senderError;
-  }
-
-  const sender = senderData as Sender;
-
-  const { data: groupParticipants, error: participantsError } = await supabase
-    .from("group_profile")
-    .select("profile_id")
-    .eq("group_id", challenge.group_id)
-    .neq("profile_id", sender_id);
-
-  console.log("Group participants", groupParticipants);
-  if (participantsError) {
-    console.error("Error participants", participantsError);
-    throw participantsError;
-  }
+  const profile = derkapData.profile as unknown as Profile;
 
   const { data: subscriptionsData, error: subscriptionsError } = await supabase
     .from("notification_subscription")
     .select("expo_push_token")
-    .in(
-      "user_id",
-      groupParticipants.map((participant) => participant.profile_id),
-    );
+    .eq("user_id", allowed_user_id)
+    .single();
 
   if (subscriptionsError) {
     console.error("Error subscriptions", subscriptionsError);
     throw subscriptionsError;
   }
 
-  const subscriptions = subscriptionsData as Subscription[];
+  const subscriptions = subscriptionsData as unknown as Subscription;
 
-  const title = `${challenge?.group?.name || "Derkap"}`;
-  const subtitle = "Nouveau post";
-  const message = `${sender.username} a pris son Derkap !`;
+  const title = profile?.username ?? "Derkap";
+  const subtitle = "Nouveau Derkap";
+  const message = `${profile?.username} a posté un nouveau Derkap !`;
 
   // const EXPO_ACCESS_TOKEN = Deno.env.get("EXPO_ACCESS_TOKEN")!;
 
-  const promises = subscriptions.map((sub: Subscription) =>
-    sendNotification({
-      expoPushToken: sub.expo_push_token,
-      title,
-      subtitle,
-      message,
-      // EXPO_ACCESS_TOKEN: EXPO_ACCESS_TOKEN,
-    }),
-  );
+  if (!subscriptions.expo_push_token) {
+    console.error("No expo push token found");
+    return new Response(JSON.stringify({ success: true }), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 
-  await Promise.allSettled(promises);
+  await sendNotification({
+    expoPushToken: subscriptions.expo_push_token,
+    title,
+    subtitle,
+    message,
+    // EXPO_ACCESS_TOKEN: EXPO_ACCESS_TOKEN,
+  });
 
   return new Response(JSON.stringify({ success: true }), {
     headers: { "Content-Type": "application/json" },
